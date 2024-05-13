03d3633e4cbad190536ea4e82098fbd94d57e5fe
Vovsanka <drobitkovolodya@gmail.com>
2024-05-13 04:14:31 +0200

    generate certificate for mec strongly connected

--- switss/certification/mec_certificates.py
+++ switss/certification/mec_certificates.py
@@ -2,13 +2,15 @@ import numpy as np
 
 from switss.solver import LP
 from switss.model import AbstractMDP
+from collections import deque
 
 
 def check_mec_certificate(amdp : AbstractMDP, mecs, mec_certificate, tol=1e-6):
     """
     """
     mec_quotient_ec_free_cert, _ = mec_certificate
-
+    
+    ### check the certificate for mec quotient ec-freeness
     # get mec quotient mdp
     q_mdp, original_inner_action_codes = amdp.mec_quotient_mdp(mecs)
     # get system matrix of the mec quotient mdp
@@ -20,13 +22,14 @@ def check_mec_certificate(amdp : AbstractMDP, mecs, mec_certificate, tol=1e-6):
         if M[(i, 0)] + tol < 1:
             return False
 
-    #
+    ### check the certificate for mec being strongly connected
 
     return True
 
 def generate_mec_certificate(amdp : AbstractMDP, mecs, certificate_bounds=1e9):
     """
     """
+    ### generate the certificate for mec quotient ec-freeness
     # get mec quotient mdp
     q_mdp, original_inner_action_codes = amdp.mec_quotient_mdp(mecs)
     # get system matrix of the mec quotient mdp
@@ -44,12 +47,63 @@ def generate_mec_certificate(amdp : AbstractMDP, mecs, certificate_bounds=1e9):
     # calculate the variables in the certificate for EC=freeness of the quotient mdp
     lp = LP.from_coefficients(A,b,opt,sense=">=")
     result = lp.solve(solver="gurobi")
+    # provide certificate
     mec_quotient_ec_free_cert = result.result_vector
 
-    #
+    ### generate the certificate for mec being strongly connected
+    vertex_count = amdp.P.shape[1]
+    # init adjacency lists
+    adj = {}  # adjacency list, contains only edges from inner actions
+    rev_adj = {} # reversed adjacency list
+    for u in range(vertex_count):
+        adj[u] = []
+        rev_adj[u] = []
+    # calculate the ajacency lists only from inner actions
+    original_inner_action_codes = set(original_inner_action_codes)
+    components,_,_ = mecs
+    for code, v_state in list(amdp.P.keys()):
+        if code not in original_inner_action_codes:
+            continue
+        u_state, action = amdp.index_by_state_action.inv[code]
+        if components[u_state] == components[v_state]:
+            adj[u_state].append(v_state)
+            rev_adj[v_state].append(u_state)
+    # init the certifying functions fwd, bwd
+    fwd = np.zeros(vertex_count, dtype=int)
+    bwd = np.zeros(vertex_count, dtype=int)
+    # processed vertices have once been visited in bfs
+    processed = np.zeros(vertex_count, dtype=bool)
+    # breadth first seach with arbitrary adjacency list and callback function vertices
+    def bfs(adjacent, source, cb):
+        visited = np.zeros(vertex_count, dtype=bool)
+        distance = np.zeros(vertex_count, dtype=int)
+        queue = deque([source])
+        visited[source] = processed[source] = True
+        while len(queue) > 0:
+            current_vertex = queue.popleft()
+            cb(current_vertex, distance[current_vertex])
+            for next_vertex in adjacent[current_vertex]:
+                if visited[next_vertex]:
+                    continue
+                visited[next_vertex] = processed[next_vertex] = True
+                distance[next_vertex] = distance[current_vertex] + 1
+                queue.append(next_vertex)
+    # functions to be called in bfs
+    def set_fwd(vertex, dist):
+        fwd[vertex] = dist
+    def set_bwd(vertex, dist):
+        bwd[vertex] = dist
+    # process all vertices with bfs and calculate fwd and bwd
+    for u in range(vertex_count):
+        if processed[u]:
+            continue
+        bfs(adj, u, set_bwd)
+        bfs(rev_adj, u, set_fwd)
+    # provide certificate
+    mec_strongly_connected_cert = (fwd, bwd)
 
-
-    mec_certificate = (mec_quotient_ec_free_cert, [])
+    # unite certificates
+    mec_certificate = (mec_quotient_ec_free_cert, mec_strongly_connected_cert)
     assert check_mec_certificate(amdp, mecs, mec_certificate)
     return mec_certificate
 
