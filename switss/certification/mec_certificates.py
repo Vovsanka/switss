68cd04b7c19a940d8635c5d85ce560edc5fdda60
Vovsanka <drobitkovolodya@gmail.com>
2024-05-21 03:12:06 +0200

    use default solver for big systems!!! & add big system example

--- switss/certification/mec_certificates.py
+++ switss/certification/mec_certificates.py
@@ -23,57 +23,60 @@ def check_mec_certificate(amdp : AbstractMDP, mecs, mec_certificate, tol=1e-6):
         :return: True if certificate is valid, False otherwise
         :rtype: bool
     """
-    mec_quotient_ec_free_cert, mec_strongly_connected_cert = mec_certificate
-    
-    ### check the certificate for mec quotient ec-freeness
-    # get mec quotient mdp
-    q_mdp, original_inner_action_codes = amdp.mec_quotient_mdp(mecs)
-    # get system matrix of the mec quotient mdp
-    q_A = q_mdp.get_system_matrix()
-    # multiply the system matrix with mec_quotient_ec_free_certificate
     try:
-        M = np.matmul(q_A, np.matrix(mec_quotient_ec_free_cert).transpose())
-    except:
-        return False
-    # check if the multiplication results fulfils >= 1 for each constraint
-    for i in range(M.shape[0]):
-        if M[(i, 0)] + tol < 1:
+        mec_quotient_ec_free_cert, mec_strongly_connected_cert = mec_certificate
+        
+        ### check the certificate for mec quotient ec-freeness
+        # get mec quotient mdp
+        q_mdp, original_inner_action_codes = amdp.mec_quotient_mdp(mecs)
+        # get system matrix of the mec quotient mdp
+        q_A = q_mdp.get_system_matrix()
+        # multiply the system matrix with mec_quotient_ec_free_certificate
+        try:
+            M = np.matmul(q_A, np.matrix(mec_quotient_ec_free_cert).transpose())
+        except:
+            return False
+        # check if the multiplication results fulfils >= 1 for each constraint
+        for i in range(M.shape[0]):
+            if M[(i, 0)] + tol < 1:
+                return False
+
+        ### check the certificate for mec being strongly connected
+        vertex_count = amdp.P.shape[1]
+        # retrieve fwd and bwd from certificate
+        fwd, bwd = mec_strongly_connected_cert
+        # check fwd, bwd are functions: S -> N
+        if fwd.shape[0] != vertex_count or bwd.shape[0] != vertex_count:
+            return False
+        if not np.issubdtype(fwd.dtype, np.integer) or not np.issubdtype(bwd.dtype, np.integer):
+            return False
+        # the fwd and the bwd constraints are fulfiled by vertex
+        fwd_ok = np.zeros(vertex_count, dtype=bool)
+        bwd_ok = np.zeros(vertex_count, dtype=bool)
+        # check each inner action
+        components,_,mec_counter = mecs
+        for code, v_state in list(amdp.P.keys()):
+            if code not in original_inner_action_codes:
+                continue
+            u_state, action = amdp.index_by_state_action.inv[code]
+            if fwd[v_state] < fwd[u_state]:
+                fwd_ok[u_state] = True
+            if bwd[u_state] < bwd[v_state]:
+                bwd_ok[v_state] = True
+        # all elements except of component leaders (bwd[u] = fwd[u] = 0) must be True
+        if not np.all(fwd_ok == bwd_ok):  
+            return False
+        # check leaders-components bijection (each component has exactly one leader)
+        component_leaders = np.zeros(mec_counter, dtype=int)
+        for state,ok in enumerate(fwd_ok):
+            if not ok:  # then state is a leader
+                component_leaders[components[state]] += 1
+        if not (component_leaders == 1).all():
             return False
 
-    ### check the certificate for mec being strongly connected
-    vertex_count = amdp.P.shape[1]
-    # retrieve fwd and bwd from certificate
-    fwd, bwd = mec_strongly_connected_cert
-    # check fwd, bwd are functions: S -> N
-    if fwd.shape[0] != vertex_count or bwd.shape[0] != vertex_count:
+        return True
+    except:
         return False
-    if not np.issubdtype(fwd.dtype, np.integer) or not np.issubdtype(bwd.dtype, np.integer):
-        return False
-    # the fwd and the bwd constraints are fulfiled by vertex
-    fwd_ok = np.zeros(vertex_count, dtype=bool)
-    bwd_ok = np.zeros(vertex_count, dtype=bool)
-    # check each inner action
-    components,_,mec_counter = mecs
-    for code, v_state in list(amdp.P.keys()):
-        if code not in original_inner_action_codes:
-            continue
-        u_state, action = amdp.index_by_state_action.inv[code]
-        if fwd[v_state] < fwd[u_state]:
-            fwd_ok[u_state] = True
-        if bwd[u_state] < bwd[v_state]:
-            bwd_ok[v_state] = True
-    # all elements except of component leaders (bwd[u] = fwd[u] = 0) must be True
-    if not np.all(fwd_ok == bwd_ok):  
-        return False
-    # check leaders-components bijection (each component has exactly one leader)
-    component_leaders = np.zeros(mec_counter, dtype=int)
-    for state,ok in enumerate(fwd_ok):
-        if not ok:  # then state is a leader
-            component_leaders[components[state]] += 1
-    if not (component_leaders == 1).all():
-        return False
-
-    return True
 
 def generate_mec_certificate(amdp : AbstractMDP, mecs):
     """Generates the MEC-decomposition certificate.
@@ -101,7 +104,7 @@ def generate_mec_certificate(amdp : AbstractMDP, mecs):
     opt = np.array(variables*[0])
     # calculate the variables in the certificate for EC=freeness of the quotient mdp
     lp = LP.from_coefficients(A,b,opt,sense=">=")
-    result = lp.solve(solver="gurobi")
+    result = lp.solve()
     # provide certificate
     mec_quotient_ec_free_cert = result.result_vector
 
